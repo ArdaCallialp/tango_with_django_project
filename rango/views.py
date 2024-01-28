21c61bfd66f33f4fee929d3d5c44217e818d3753
“Arda <2795083c@student.gla.ac.uk>
2024-01-28 21:06:06 +0000

    chapter 3 new hyperlinks added

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    about_link = '<a href="/rango/about/">About</a>'
+    return HttpResponse(f"Rango says hey there partner! Visit the {about_link}.")
+
+def about(request):
+    index_link = '<a href="/rango/">Index</a>'
+    return HttpResponse(f"Rango says here is the about page. Visit the {index_link}.")
