8405e546f3064fa03c0cfa6c050ca1c8fdc07ccb
“Arda <2795083c@student.gla.ac.uk>
2024-01-31 18:46:00 +0000

    chapter 5 implementations have been added

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')  # Corrected field name to 'Title'
+
+# Register the models with the custom admin classes
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
